d3bbaf40b694e32c6d0189413bd2d20894a21495
kdh <dongri.kang@sansumg.com>
2025-04-14 09:22:36 +0900

    [feature] 글자 수 check - 60

---
+++ Project1/CopyCheck.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <stdexcept>
+
+using std::string;
+
+class CopyCheck
+{
+public:
+    int CheckNumOfLetter(string str1, string str2)
+    {
+        return 60;
+    }
+private:
+};
