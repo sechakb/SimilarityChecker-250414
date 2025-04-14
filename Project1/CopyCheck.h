e06cb77127cf7f6afc8c070653c320e65e52f338
kdh <dongri.kang@sansumg.com>
2025-04-14 09:52:14 +0900

    [refactoring] 리뷰 반영

--- Project1/CopyCheck.h
+++ Project1/CopyCheck.h
@@ -4,12 +4,29 @@
 
 using std::string;
 
+#define IN
+#define OUT
+
 class CopyCheck
 {
 public:
     int CheckNumOfLetter(string str1, string str2)
     {
         int longer, shorter;
+
+        GetLongerAndShorter(longer, shorter, str1, str2);
+
+        int score = (2 * shorter - longer) * 60 / shorter;
+        if (score < 0)
+        {
+            score = 0;
+        }
+
+        return score;
+    }
+private:
+    void GetLongerAndShorter(OUT int &longer, OUT int &shorter, IN const string &str1, IN const string &str2)
+    {
         int length1 = str1.length();
         int length2 = str2.length();
 
@@ -23,14 +40,5 @@ public:
             longer = length2;
             shorter = length1;
         }
-
-        int score = (2 * shorter - longer) * 60 / shorter;
-        if (score < 0)
-        {
-            score = 0;
-        }
-
-        return score;
     }
-private:
 };
