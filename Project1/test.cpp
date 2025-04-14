92912416af8bfbfbcc4c8763b530dfab985d20f5
kdh <dongri.kang@sansumg.com>
2025-04-14 10:15:55 +0900

    알파벳 check

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -28,3 +28,25 @@ TEST(TS0, CheckNumOfLetterScore30)
 
     EXPECT_EQ(30, app.CheckNumOfLetter("ABC", "DB"));
 }
+
+TEST(TS0, CheckAlphabet40)
+{
+    CopyCheck app;
+
+    EXPECT_EQ(40, app.CheckAlphabet("ASD", "DSA"));
+    EXPECT_EQ(40, app.CheckAlphabet("AAABB", "BA"));
+}
+
+TEST(TS0, CheckAlphabet0)
+{
+    CopyCheck app;
+
+    EXPECT_EQ(0, app.CheckAlphabet("ASD", "BB"));
+}
+
+TEST(TS0, CheckAlphabet20)
+{
+    CopyCheck app;
+
+    EXPECT_EQ(20, app.CheckAlphabet("AA", "AAE"));
+}
