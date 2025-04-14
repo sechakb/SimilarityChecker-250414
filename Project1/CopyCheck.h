a718b2f760d6a8ac56525f8d4c70ba9bbeca1212
kdh <dongri.kang@sansumg.com>
2025-04-14 09:30:36 +0900

    [refactoring] 글자 수 check

--- Project1/CopyCheck.h
+++ Project1/CopyCheck.h
@@ -9,7 +9,28 @@ class CopyCheck
 public:
     int CheckNumOfLetter(string str1, string str2)
     {
-        return 60;
+        int longer, shorter;
+        int length1 = str1.length();
+        int length2 = str2.length();
+
+        if (length1 > length2)
+        {
+            longer = length1;
+            shorter = length2;
+        }
+        else
+        {
+            longer = length2;
+            shorter = length1;
+        }
+
+        int score = (2 * shorter - longer) * 60 / shorter;
+        if (score < 0)
+        {
+            score = 0;
+        }
+
+        return score;
     }
 private:
 };
